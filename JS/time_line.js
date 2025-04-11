69097a00abde90afe135b7b3159c78699b41fddb
DiegoWhite99 <diegofer.cas.99@gmail.com>
2025-04-11 14:02:53 -0500

    se reliza time line en support

---
+++ JS/time_line.js
@@ -0,0 +1,4 @@
+document.getElementById('ingresar-btn').addEventListener('click', () => {
+    document.getElementById('timeline-container').classList.remove('hidden');
+    document.getElementById('form-etapa1').classList.remove('hidden');
+  });
