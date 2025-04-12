3feda470ec512b510247a83bd436f736edeaf7b8
juanospina0409 <ospinajuan0409@gmail.com>
2025-04-12 15:44:23 -0500

    Se realizan y se trasladan algunos cambios en el CSS de support y los scripts se trasladan a nuevo archivo JS de support, se renombran y se eliminan algunos archivos JS

--- JS/time_line.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('ingresar-btn').addEventListener('click', () => {
-    document.getElementById('timeline-container').classList.remove('hidden');
-    document.getElementById('form-etapa1').classList.remove('hidden');
-  });
