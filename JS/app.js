d901befb3ee5481fb3d4f160c72f3a4adb24b1fc
Juan Sebastian Ospina chavez <ospinajuan0409@gmail.com>
2025-04-15 14:18:41 -0500

    Se cambia el color primario azul a un tono mas claro, tambien para particulas en servicios

--- JS/app.js
+++ JS/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ea06ff"
+        "value": "#0dcaf0"
       },
       "shape": {
         "type": "triangle",
         "stroke": {
           "width": 0,
-          "color": "#ea06ff"
+          "color": "#0dcaf0"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ea06ff",
+        "color": "#0dcaf0",
         "opacity": 0.4,
         "width": 1
       },
